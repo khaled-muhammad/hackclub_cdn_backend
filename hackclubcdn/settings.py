d7a9a52d45b1c13901f888b3339d1ef90cd50ed0
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-20 00:46:02 +0300

    added trusted CSRF

--- hackclubcdn/settings.py
+++ hackclubcdn/settings.py
@@ -178,3 +178,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://hackclub-cdn.khaled.hackclub.app",
+]
