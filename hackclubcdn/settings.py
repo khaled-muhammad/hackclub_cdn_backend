cb4922e524c4e9fb7b25c6dccac3126dc5f6feb7
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-20 00:43:42 +0300

    test middleware order change

--- hackclubcdn/settings.py
+++ hackclubcdn/settings.py
@@ -57,9 +57,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
