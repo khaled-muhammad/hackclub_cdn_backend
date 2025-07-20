a1f1ffe6aa62bfc37bb38f00f40c3cdbacfafe05
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-20 15:30:51 +0300

    fix bug

--- hackclubcdn/settings.py
+++ hackclubcdn/settings.py
@@ -177,7 +177,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = True
 
 CSRF_TRUSTED_ORIGINS = [
     "https://hackclub-cdn.khaled.hackclub.app",
