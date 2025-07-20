e1cb93472ff41327ed5380acb5d0339bf9dcfa88
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-20 22:00:01 +0300

    update template structure

--- hackclubcdn/settings.py
+++ hackclubcdn/settings.py
@@ -171,7 +171,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR / "templates"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
