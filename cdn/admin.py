d69017983b3a448748676af6d8fd3faef0998d22
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-24 14:21:29 +0300

    finished CDN APP

---
+++ cdn/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import *
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['filename', 'owner', 'file_size_human', 'mime_type', 'created_at']
+    list_filter = ['mime_type', 'processing_status']
+    search_fields = ['filename', 'original_filename', 'owner__user__email']
+    readonly_fields = ['id', 'md5_hash', 'sha256_hash', 'created_at', 'updated_at']
+
+admin.site.register(Folder)
+admin.site.register(TrashItem)
+
+# @admin.register(Share)
+# class ShareAdmin(admin.ModelAdmin):
+#     list_display = ['resource_type', 'owner', 'permission_level', 'is_public', 'expires_at']
+#     list_filter = ['resource_type', 'permission_level', 'is_public']
+#     search_fields = ['owner__email', 'public_token']
