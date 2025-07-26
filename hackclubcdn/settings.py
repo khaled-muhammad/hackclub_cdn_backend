c30f9187cbab2f6b10dd23db7404b4007eefc6b5
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-26 11:01:27 +0300

    deleted default host and port for Nest

--- hackclubcdn/settings.py
+++ hackclubcdn/settings.py
@@ -103,8 +103,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', default='localhost'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
